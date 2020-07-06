ee6f65dfe1f1965a19427986023b4da420d84fd3
tvaidotas <tadas.vaidotas@gmail.com>
2020-07-06 16:46:34 +0100

    added layout and made use of it

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello Everyone!!!!!!!!!'
+    return render_template('homepage.html', title='Homepage')
+
+
+@app.route('/about')
+def about():
+    return '''
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>About Page</title>
+    </head>
+    <body>
+        <h1>This about page</h1>
+    </body>
+    </html>
+    '''
 
 
 if __name__ == '__main__':
